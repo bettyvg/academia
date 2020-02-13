fd51733b10a620fdbbb77bf0b0bc9d945e78559a
bescamilla <48139844+bescamilla@users.noreply.github.com>
2020-02-13 13:10:37 -0600

    prueba

---
+++ app/Http/Controllers/ReportesController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bryan.escamilla
+ * Date: 13/02/2020
+ * Time: 01:09 PM
+ */
+
+namespace App\Http\Controllers;
+
+class ReportesController extends Controller{
+    public function ok(){
+
+    }
+}
+
+?>
